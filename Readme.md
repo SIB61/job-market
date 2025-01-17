2b8dabafb958428a991940377586ab87c846da01
Md Sabit Islam Bhuiya <sib.sustswe@gmail.com>
2025-01-18 03:04:25 +0600

    added docker compose setup. added readme file.

---
+++ Readme.md
@@ -0,0 +1,24 @@
+## SETUP INSTRUCTION
+
+- Clone the repository
+- [Install docker](https://docs.docker.com/engine/install/) with docker-compose-plugin.
+
+### start the app
+
+open terminal into the project repository and run the command below
+
+```bash
+sudo docker compose build && sudo docker compose up -d
+```
+
+### see the logs
+
+```bash
+sudo docker compose logs -f app
+```
+
+### stop the app:
+
+```bash
+sudo docker compose down
+```
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
 import "module-alias/register";
 import { config } from "dotenv";
+config({});
+config({ path: `.env.local`, override: true });
 import connectDb from "./config/db";
 import app from "@/app";
 import logger from "./utils/logger";
 import { createServer } from "http";
 import { createIO } from "./config/socket";
 
-config({});
-config({ path: `.env.local`, override: true });
-
 const port = process.env.PORT || 3000;
-
-connectDb();
-
 const server = createServer(app);
-createIO(server);
-
+connectDb();
+createIO(server)
 server.listen(port, () => {
   logger.info(`Server is running at http://localhost:${port}`);
 });
